fee56c7a9e25ebc7f33339e811e5980ee819c076
ajaychaudhary8955 <ajaychaudhary8104@gmail.com>
2026-07-01 12:34:24 +0530

    add data ingestion training pipeline

---
+++ main.py
@@ -0,0 +1,13 @@
+from src.demand_forecasting_and_inventory_optimization import logger
+from src.demand_forecasting_and_inventory_optimization.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+
+
+STAGE_NAME = "Data Ingestion stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
